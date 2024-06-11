ec8ce6125f2baae529983841bdb2c00f33e2026b
realwumbl3 <wumbl3@gmail.com>
2024-06-10 21:06:52 -0400

    same files for chrome extension / webui-extension

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", async () => {
 	try {
-		await import(chrome.runtime.getURL("./src/main.js"));
+		await import(chrome.runtime.getURL("./javascript/src/main.js"));
 	} catch (error) {
 		console.error("[Error importing src/main.js]", error);
 	}
