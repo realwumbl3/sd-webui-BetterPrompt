96a2a59f6a482a9aaeceb2e9f9d67b74c44577cf
realwumbl3 <wumbl3@gmail.com>
2024-06-22 19:39:41 -0400

    web dev is hell

--- javascript/src/nodes/tags.js
+++ javascript/src/nodes/tags.js
@@ -22,7 +22,6 @@ export default class TagsNode extends Node {
         this.add_tag.addEventListener('click', () => this.addTag(''))
 
         const value = super.getJson().value
-
         if (value) {
             for (const tag of value) {
                 this.addTag(tag)
@@ -30,8 +29,17 @@ export default class TagsNode extends Node {
         }
     }
 
+    removeTag(tag) {
+        const previousTag = this.tags[this.tags.indexOf(tag) - 1]
+        this.tags.remove(tag)
+        tag.main.remove()
+        setTimeout(() => previousTag?.focus(), 10)
+    }
+
     addTag(tag) {
-        this.tags.push(new Tag(this, tag))
+        const newTag = new Tag(this, tag)
+        this.tags.push(newTag)
+        return newTag
     }
 
     toPrompt() {
@@ -55,31 +63,46 @@ class Tag {
         this.tagNode = tagNode
         this.value = value
 
-        this.autoFitInput = new AutoFitInput()
+        this.input = new AutoFitInput()
+        this.input.addEventListener('input', () => this.updateTag())
+        this.input.addEventListener('keydown', (e) => {
+            // if enter is pressed, add a new tag
+            if (e.key === 'Enter') {
+                this.tagNode.addTag('')
+            }
+            if (e.key === 'Backspace' && this.input.value() === '') {
+                if (this.tagNode.tags.length > 1) this.removeTag()
+            }
+        })
 
         html`
             <div this=main class="Tag">
-                ${this.autoFitInput}
+                ${this.input}
                 <div class="Remove" this="remove">X</div>
             </div>
         `.bind(this)
 
-        this.autoFitInput.input.value = this.value
+        this.input.set(this.value)
 
         this.remove.addEventListener('click', () => this.removeTag())
-        this.autoFitInput.input.addEventListener('input', () => this.updateTag())
 
-        setTimeout(() => this.autoFitInput.updateWidth(), 100)
+    }
 
+    focus() {
+        this.input.focus()
     }
 
     removeTag() {
-        this.tagNode.tags.remove(this)
-        this.main.remove()
+        this.tagNode.removeTag(this)
     }
 
     updateTag() {
-        this.value = this.autoFitInput.input.value
+        this.value = this.input.value()
+    }
+
+    onConnected() {
+        this.input.updateWidth()
+        this.input.focus()
     }
 
 }
@@ -95,8 +118,27 @@ class AutoFitInput {
         `.bind(this)
         this.input.addEventListener('input', () => this.updateWidth())
     }
+
+    focus() {
+        this.input.focus()
+    }
+
+    addEventListener(...args) {
+        this.input.addEventListener(...args)
+    }
+
+    set(value) {
+        this.input.value = value
+    }
+
+    value() {
+        return this.input.value
+    }
+
     updateWidth() {
-        this.span.textContent = this.input.value < 1 ? this.input.placeholder : this.input.value
-        this.input.style.width = this.span.offsetWidth + 10 + 'px'
+        setTimeout(() => {
+            this.span.textContent = this.input.value < 1 ? this.input.placeholder : this.input.value
+            this.input.style.width = this.span.offsetWidth + 10 + 'px'
+        }, 10)
     }
 }
